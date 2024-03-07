0f2e5ea317c0bb5cf2b2d534228351913de8cf80
Chris <chris35469@gmail.com>
2024-03-07 14:42:56 -0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    basePath: '/frame1',
+    output: 'export',
+    assetPrefix: "/frame1"
+}
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/frame1',
+    assetPrefix: "/frame1"
+};
+
+export default nextConfig;
